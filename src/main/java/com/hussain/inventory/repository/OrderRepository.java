dcdb253534ebeb7304d318751883a5d7fddbdb02
Mir Hussain Ali <112266585+Hussain1204@users.noreply.github.com>
2026-04-02 07:23:07 +0530

    Update OrderRepository.java to implement custom query methods

--- src/main/java/com/hussain/inventory/repository/OrderRepository.java
+++ src/main/java/com/hussain/inventory/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -14,6 +16,7 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     @EntityGraph(attributePaths = {"customer", "items", "items.product"})
     Page<OrderEntity> findAll(Pageable pageable);
 
+    @Query("SELECT o FROM OrderEntity o WHERE o.id = :id")
     @EntityGraph(attributePaths = {"customer", "items", "items.product"})
-    Optional<OrderEntity> findWithDetailsById(Long id);
+    Optional<OrderEntity> findWithDetailsById(@Param("id") Long id);
 }
